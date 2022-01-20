46c738699a75856f61ddf66edba10a17ab3580a6
yuxiaotu <yuxiaotu1024@qq.com>
2022-01-20 15:07:56 +0800

    ppt

---
+++ src/js/2th.js
@@ -0,0 +1,33 @@
+var clientHight = window.innerHeight;
+var index = 0;
+var flag = true;
+var box = document.querySelector('.box');
+var itemLen = document.querySelectorAll('.item').length;
+
+const slide = function(index) {
+  box.style.transform = 'translateY(' + (- index * clientHight) + 'px)';
+}
+
+window.addEventListener('mousewheel', e => {
+  if (flag) {
+    flag = false;
+    if (e.wheelDelta < 0) {
+      if (index === itemLen - 1) {
+        index -= 1;
+        flag = true;
+      }
+      index++;
+    } else {
+      if (index === 0) {
+        index = 0;
+        flag = true;
+      } 
+      index--;
+    }
+    slide(index);
+  }
+});
+
+box.addEventListener('transitionend', e => {
+  flag = true;
+})
